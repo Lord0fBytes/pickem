cbf78326903d73d3984904029729ed07bef6838b
Webtron18 <jmasse@webtron.me>
2017-04-17 22:53:57 -0400

    added some cookie fun to the mix

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(cookieParser());
 
 app.get('/first_template', function(req, res){
   res.render('first_view');
@@ -10,6 +12,14 @@ app.get('/first_template', function(req, res){
 app.get('/dynamic_template/:id', function(req, res){
   res.render('dynamic', {
     idNum: req.params.id
+  });
+});
+app.get('/cookie', function(req,res){
+  res.cookie('username', 'jmasse').send('cookies set');
+});
+app.get('/cookie/get', function(req,res){
+  res.render('dynamic', {
+    idNum: req.cookies.username
   });
 });
 
